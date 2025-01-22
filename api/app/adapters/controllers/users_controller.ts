38797ec3a8dd4b36075b8f0e23a96fe6ad81f481
gabyorel <146829386+gabyorel@users.noreply.github.com>
2025-01-22 22:40:50 +0100

    Implement Adonis authentication with OAT

--- api/app/adapters/controllers/users_controller.ts
+++ api/app/adapters/controllers/users_controller.ts
@@ -1,7 +1,7 @@
 import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
 import { UserService } from '../../domain/services/user_service.js'
-import { userValidator } from '../validators/user_validator.js'
+import { registerValidator } from '../validators/register_validator.js'
 import { CreateUserDto } from '../../application/dtos/create_user_dto.js'
 
 @inject()
@@ -10,7 +10,7 @@ export default class UsersController {
 
   public async create({ response, request }: HttpContext) {
     const userDto: CreateUserDto = request.only(['email', 'password'])
-    const payload = await userValidator.validate(userDto)
+    const payload = await registerValidator.validate(userDto)
     return response.send(await this.userService.create())
   }
 }
--- api/start/routes.ts
+++ api/start/routes.ts
@@ -1,8 +1,24 @@
 import router from '@adonisjs/core/services/router'
-const UsersController = () => import('../app/adapters/controllers/users_controller.js')
+import { middleware } from './kernel.js'
+
+const AuthController = () => import('../app/adapters/controllers/auth_controller.js')
 
 router
   .group(() => {
-    router.post('/register', [UsersController, 'create'])
+    router.post('/register', [AuthController, 'register'])
+    router.post('/login', [AuthController, 'login'])
+    router
+      .post('/logout', [AuthController, 'logout'])
+      .use(middleware.auth())
   })
   .prefix('user')
+
+router.get('me', async ({ auth, response }) => {
+  try {
+    const user = auth.getUserOrFail()
+    return response.ok(user)
+  } catch (error) {
+    return response.unauthorized({ error: 'User not found' })
+  }
+})
+.use(middleware.auth())
