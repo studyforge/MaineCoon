83b3b7dbf672c56e31635b2eff0b6e23b0a2f8f0
gabyorel <146829386+gabyorel@users.noreply.github.com>
2025-01-30 16:30:20 +0100

    Add terraform workflow

---
+++ config/terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    docker = {
+      source = "docker/docker"
+      version = "~> 0.4"
+    }
+  }
+
+  required_version = ">= 1.10.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "docker" {}
+
+resource "docker_hub_repository" "mainecoon-app" {
+  namespace = var.namespace
+  name      = "mainecoon-app"
+  private   = false
+}
