1314b6d20f1a3045b48fb6aec0443091df27053f
SKR4VV <>
2025-02-06 16:39:38 +0100

    build: create compose stack for dev

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.VITE_PORT || 5173) 
+  }
 })
