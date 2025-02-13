b21a5ed818f6587d041891d48753feebbc62daa3
Axel Ruelle <axel.ruelle@exotec.com>
2025-02-13 10:28:42 +0100

    rds fix multi-az

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -82,6 +82,8 @@ resource "aws_db_instance" "mainecoon-db" {
   username             = var.rds_username
   password             = var.rds_password
   publicly_accessible  = false
+  multi_az             = false
+  availability_zone   = "us-east-1b"
   db_subnet_group_name = aws_db_subnet_group.mainecoon-subnet_group.name
   vpc_security_group_ids = [aws_security_group.mainecoon-sg-db.id]
 
