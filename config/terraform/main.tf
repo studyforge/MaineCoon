53887cb9a13f0aea3a5c59d0507f937830dea6a0
SKR4VV <>
2025-02-13 11:47:32 +0100

    le bordel d'axel

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -23,14 +23,27 @@ resource "aws_internet_gateway" "mainecoon-igw" {
   }
 }
 
-# Création d'un subnet privé
-resource "aws_subnet" "mainecoon-subnet" {
+# Création d'un subnet privé #1
+resource "aws_subnet" "mainecoon-subnet-1" {
   vpc_id                  = aws_vpc.mainecoon-vpc.id
   cidr_block              = "172.16.1.0/24"
 
   map_public_ip_on_launch = false
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "mainecoon-subnet"
+    Name = "mainecoon-subnet-1"
+  }
+}
+
+# Création d'un subnet privé #2
+resource "aws_subnet" "mainecoon-subnet-2" {
+  vpc_id                  = aws_vpc.mainecoon-vpc.id
+  cidr_block              = "172.16.2.0/24"
+
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "mainecoon-subnet-2"
   }
 }
 
@@ -62,12 +75,12 @@ resource "aws_security_group" "mainecoon-sg-db" {
 }
 
 # Créer un groupe de sous-réseaux pour la DB
-resource "aws_db_subnet_group" "mainecoon-subnet_group" {
-  name       = "my-subnet-group"
-  subnet_ids = [aws_subnet.mainecoon-subnet.id]
+resource "aws_db_subnet_group" "mainecoon-subnet_group-1" {
+  name       = "mainecoon-subnet_group-1"
+  subnet_ids = [aws_subnet.mainecoon-subnet-1.id, aws_subnet.mainecoon-subnet-2.id]
 
   tags = {
-    Name = "mainecoon-subnet_group"
+    Name = "mainecoon-subnet_group-1"
   }
 }
 
@@ -75,7 +88,7 @@ resource "aws_db_subnet_group" "mainecoon-subnet_group" {
 resource "aws_db_instance" "mainecoon-db" {
   instance_class       = "db.t3.micro"
   storage_type         = "gp3"
-  allocated_storage    = 10
+  allocated_storage    = 20
   engine               = "postgres"
   engine_version       = "17.2"
   db_name              = var.rds_db_name
@@ -83,8 +96,7 @@ resource "aws_db_instance" "mainecoon-db" {
   password             = var.rds_password
   publicly_accessible  = false
   multi_az             = false
-  availability_zone   = "us-east-1b"
-  db_subnet_group_name = aws_db_subnet_group.mainecoon-subnet_group.name
+  db_subnet_group_name = aws_db_subnet_group.mainecoon-subnet_group-1.name
   vpc_security_group_ids = [aws_security_group.mainecoon-sg-db.id]
 
   tags = {
