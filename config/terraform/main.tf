7a59bafc026141233e796e91445ca3504dc27a3a
sartek430 <macogneur430@gmail.comm>
2025-02-13 10:07:35 +0100

    update terraform

--- config/terraform/main.tf
+++ config/terraform/main.tf
@@ -90,3 +90,12 @@ resource "aws_db_instance" "mainecoon-db" {
     Name = "mainecoon-db"
   }
 }
+
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnet[*].id
+  }
+}
