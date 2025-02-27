62ab461d1412861e0dc81d3b040aeed2abd3e551
gabyorel <146829386+gabyorel@users.noreply.github.com>
2025-02-27 15:26:47 +0100

    Import existing resources with import blocks

--- config/terraform/providers.tf
+++ config/terraform/providers.tf
@@ -16,3 +16,23 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.mainecoon.kube_config[0].cluster_ca_certificate)
   token                  = data.digitalocean_kubernetes_cluster.mainecoon.kube_config[0].token
 }
+
+import {
+  id = "07daeb87-719e-41db-be57-14b04eae3f73"
+  to = digitalocean_vpc.mainecoon
+}
+
+import {
+  id = "b8ecd2ab-2267-4a5e-8692-cbf1d32583e3"
+  to = digitalocean_database_firewall.k8s_access
+}
+
+import {
+  id = "c416ce8e-f731-4521-b7ab-54877ab55ec6"
+  to = digitalocean_kubernetes_cluster.mainecoon
+}
+
+import {
+  id = "4aa687c8-7517-4c58-af03-455d85a13360"
+  to = digitalocean_database_cluster.mainecoon
+}
