9186d710ae541026df81f9d3589ab001f9182533
sartek430 <macogneur430@gmail.comm>
2025-02-28 14:22:44 +0100

    gestion d'erreur addUserToConversation

--- api/app/infrastructure/repositories/pg_conversation_repository.ts
+++ api/app/infrastructure/repositories/pg_conversation_repository.ts
@@ -1,19 +1,16 @@
 import ConversationRepository from '../../domain/repositories/conversation_repository.js'
 import Conversation from '../models/conversation.js'
-import User from '../models/user.js'
 
 export default class PgConversationRepository implements ConversationRepository {
   public async create(name: string): Promise<void> {
     await Conversation.create({ name })
   }
 
-  public async findById(id: string): Promise<Conversation | null> {
+  public async findById(id: number): Promise<Conversation | null> {
     return await Conversation.find(id)
   }
 
-  public async addUserToConversation(userId: number, conversationId: number): Promise<void> {
-    const conversation = await Conversation.findOrFail(conversationId)
-    const user = await User.findOrFail(userId)
-    await conversation.related('user').attach([user.id])
+  public async addUserToConversation(userId: number, conversation: Conversation): Promise<void> {
+    await conversation.related('user').attach([userId])
   }
 }
