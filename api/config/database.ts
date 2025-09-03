c05a30be5b9bb22d941fa9f4a5a6a934a55363dc
sartek430 <macogneur430@gmail.comm>
2025-09-03 13:06:24 +0200

    fix: revert database connection settings to hardcoded values for local development chore: update caniuse-lite version in package-lock.json chore: add package-lock.json for MaineCoon project

--- api/config/database.ts
+++ api/config/database.ts
@@ -7,12 +7,12 @@ const dbConfig = defineConfig({
     postgres: {
       client: 'pg',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
-        ssl: true,
+        host: '127.0.0.1',
+        port: 5432,
+        user: "postgres",
+        password: "postgres",
+        database: "app",
+        ssl: false,
       },
       migrations: {
         naturalSort: true,
